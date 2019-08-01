f5ffe864edd0f2fb3e43103162959ed5d9a6393e
infosalim <salimh5757@gmail.com>
2019-08-02 00:52:52 +0600

    used static assets

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
+const path = require('path');
+
 const express = require('express');
 const app = express();
 
-app.get('', (req, res)=>{
-    res.send('Hello World!');
-});
 
+const publicDirectoryPath = path.join(__dirname, '../public');
+app.use(express.static(publicDirectoryPath));
+
+
+app.get('', (req, res)=>{
+    res.send('Help me!');
+});
 app.get('/salim', (req, res)=>{
     res.send('Help me please!');
 });
