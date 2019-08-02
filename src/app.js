1f7aa493513ec9ed4d755e683f9327a934c46a83
infosalim <salimh5757@gmail.com>
2019-08-03 00:46:24 +0600

    worked on templating

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 const path = require('path');
 
 const express = require('express');
+const hbs = require('hbs');
+
 const app = express();
 
-
+// Define path for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
+// customized views deirecotory
+//const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs');
+// set cutomized directory
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static direcory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res)=>{
@@ -14,8 +27,20 @@ app.get('', (req, res)=>{
         author: 'Salim Hossain'
     });
 });
-app.get('/salim', (req, res)=>{
-    res.send('Help me please!');
+
+app.get('/about', (req, res)=>{
+    res.render('about',{
+        title: 'About me',
+        name: 'Salim Hossain'
+    });
+});
+
+app.get('/help', (req, res)=>{
+    res.render('help',{
+        helpText: 'This is some helpful text!',
+        title: 'About me',
+        name: 'Salim Hossain'
+    });
 });
 
 
