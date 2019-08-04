c11a0b696d5b14ea2653a88b323dce9e6b43b36e
infosalim <salimh5757@gmail.com>
2019-08-04 23:41:06 +0600

    finished the app

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,17 +1,25 @@
 // Get data via fetch API
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
     const location = search.value;
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
     fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log(data.error);
+                messageOne.textContent = data.error
             } else {
-                console.log(data.location);
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
                 console.log(data.forecast);
             }
         })
